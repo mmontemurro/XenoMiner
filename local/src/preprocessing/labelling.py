d897466168fda32ae23739d260680e76812d6f2a
Marilisa Montemurro <mmontemurro@194.116.76.143>
2021-07-19 15:49:13 +0200

    fix

--- local/src/preprocessing/labelling.py
+++ local/src/preprocessing/labelling.py
@@ -5,13 +5,15 @@ import pandas as pd
 def main():
     parser = argparse.ArgumentParser(description="Concatenates human and murine matrices, adding the proper label")
     parser.add_argument("-h", "--human", help="Human matrix", required=True)
-    parser.add_argument("-m", "--murine", help="Murine matrix", required=True)
+    parser.add_argument("-m", "--mouse", help="Murine matrix", required=True)
     parser.add_argument("-o", "--output_file", help="Output file", required=True)
 
     args = parser.parse_args()
 
     df_h = pd.read_csv(args.human, index_col=0)
+    df_h['label'] = "h"
     df_m = pd.read_csv(args.murine, index_col=0)
+    df_m['label'] = 'm'
 
     df = pd.DataFrame(df_h, df_m)
 
